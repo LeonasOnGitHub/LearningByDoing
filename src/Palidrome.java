640d4e03be42e0fbab75ab4106f82e8bfc1c41a2
lmedem <adobe@gmx.net>
2023-09-28 13:04:45 +0200

    4. Pali Done

--- src/Palidrome.java
+++ src/Palidrome.java
@@ -2,9 +2,16 @@ public class Palidrome {
     public static void main(String[] args) {
         System.out.println(isPalindrome("aba"));
         System.out.println(isPalindrome("abab"));
+        System.out.println(isPalindrome("ababababababbaba"));
+        System.out.println(isPalindrome("asdfghhgfdsa"));
     }
     public static boolean isPalindrome(String word) {
-
-        return false;
+        char[] r = word.toCharArray();
+        for (int i = 0; i <r.length/2 ; i++) {
+            if (r[i]!=r[r.length-(i+1)]){
+                return false;
+            }
+        }
+        return true;
     }
 }
